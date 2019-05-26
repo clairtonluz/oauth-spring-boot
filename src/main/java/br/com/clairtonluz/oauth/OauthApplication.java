5864f0a1c754b1152e838963409f10c7947231e4
Clairton Luz <clairton.c.l@gmail.com>
2019-05-26 09:18:50 -0300

    login com facebook

---
+++ src/main/java/br/com/clairtonluz/oauth/OauthApplication.java
@@ -0,0 +1,15 @@
+package br.com.clairtonluz.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+
+@EnableOAuth2Sso
+@SpringBootApplication
+public class OauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthApplication.class, args);
+	}
+
+}
